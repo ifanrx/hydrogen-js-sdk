3db203505430dce149e795c6832a567540e38d9d
Ernest He <ernest.he2009@gmail.com>
2017-07-03 20:33:40 +0800

    fix uploadFile not working issue.

--- src/uploadFile.js
+++ src/uploadFile.js
@@ -1,5 +1,6 @@
 const baasRequest = require('./baasRequest').baasRequest
 const BaaS = require('./baas')
+const API_HOST = BaaS._config.API_HOST
 const API = BaaS._config.API
 const constants = require('./constants')
 const Promise = require('./promise')
@@ -11,41 +12,37 @@ const Promise = require('./promise')
  */
 
 const uploadFile = (params) => {
-  return baasRequest({
-    url: API.UPLOAD
-  }).then((res) => {
-    let token = BaaS.getAuthToken()
 
-    if (!token) {
-      let intervalID = setInterval(() => {
-        token = BaaS.getAuthToken()
-        if (token) {
-          clearInterval(intervalID)
-        }
-      }, 500)
-    }
+  if (!BaaS._config.CLIENT_ID) {
+    reject('未初始化客户端');
+  }
 
-    return new Promise((resolve, reject) => {
-      wx.uploadFile({
-        url: API.UPLOAD,
-        filePath: params.filePath,
-        name: constants.UPLOAD.UPLOAD_FILE_KEY,
-        formData: params.formData,
-        header: {
-          'Authorization': constants.UPLOAD.HEADER_AUTH_VALUE + token,
-          'X-Hydrogen-Client-ID': BaaS._config.CLIENT_ID,
-        },
-        success: (res) => {
-          return resolve(res)
-        },
-        fail: (err) => {
-          return reject(err)
-        }
-      })
+  let token = BaaS.getAuthToken()
 
-    }, (err) => {
-      throw new Error(err)
+  if(!token) {
+    return reject()
+  }
+
+  return new Promise((resolve, reject) => {
+    wx.uploadFile({
+      url: API_HOST + API.UPLOAD,
+      filePath: params.filePath,
+      name: constants.UPLOAD.UPLOAD_FILE_KEY,
+      formData: params.formData,
+      header: {
+        'Authorization': constants.UPLOAD.HEADER_AUTH_VALUE + token,
+        'X-Hydrogen-Client-ID': BaaS._config.CLIENT_ID,
+        'User-Agent': constants.UPLOAD.UA,
+      },
+      success: (res) => {
+        return resolve(res)
+      },
+      fail: (err) => {
+        return reject(err)
+      }
     })
+  }, (err) => {
+    throw new Error(err)
   })
 }
 
