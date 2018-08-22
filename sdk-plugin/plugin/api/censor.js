57fc39e4916a3a1c16436ff16f1e67caa06670f4
陈春寒 <381582176@qq.com>
2018-08-22 18:02:13 +0800

    数据表查询增加 count 方法

--- sdk-plugin/plugin/api/censor.js
+++ sdk-plugin/plugin/api/censor.js
@@ -24,7 +24,7 @@ const wxCensorImage = filePath => {
           return reject(res)
         }
 
-        resolve(data)
+        resolve(JSON.parse(data))
       },
       fail: () => {
         utils.wxRequestFail(reject)
