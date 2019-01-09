5a86c0611abd70849598b2b7f40865c13b4c388a
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-01-09 18:41:11 +0800

    alipay 挂载 polyfill 与 uploadFile

--- sdk-file/src/alipay/index.js
+++ sdk-file/src/alipay/index.js
@@ -3,11 +3,15 @@ const BaaS = require('core-module/baas')
 const auth = require('./auth')
 const request = require('./request')
 const baasRequest = require('./baasRequest')
+const polyfill = require('./polyfill')
+const uploadFile = require('./uploadFile')
 
 BaaS.use(core)
 BaaS.use(auth)
 BaaS.use(request)
 BaaS.use(baasRequest)
+BaaS.use(polyfill)
+BaaS.use(uploadFile)
 BaaS._createRequestMethod()
 
 // 暴露 BaaS 到小程序环境
