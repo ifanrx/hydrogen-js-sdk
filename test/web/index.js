c2955cd90a9225e3028ece3ce54b674adc82909a
chenchunhan <chenchunhan@ifanr.com>
2019-01-17 18:30:22 +0800

    更新 web 端测试用例

--- test/web/index.js
+++ test/web/index.js
@@ -19,30 +19,72 @@ function init() {
           email: '',
           username: '',
           password: '',
+        },
+        passwordChangeForm: {
+          password: '',
+          new_password: ''
+        },
+        userInfoForm: {
+          email: '',
+          username: '',
         }
       }
     },
     methods: {
       login() {
-        console.log(this.loginForm)
+        window.BaaS.auth.login(_.pickBy(this.loginForm, v => !!v)).then(res => {
+          console.log(res)
+        })
       },
       register() {
-
+        window.BaaS.auth.register(_.pickBy(this.registerForm, v => !!v)).then(res => {
+          console.log(res)
+        })
       },
       logout() {
-
+        window.BaaS.auth.logout().then(res => {
+          console.log(res)
+        })
       },
       forgetPwd() {
-
+        window.BaaS.auth.requestPasswordReset().then(res => {
+          console.log(res)
+        })
       },
       anonymousLogin() {
         BaaS.auth.anonymousLogin().then(res => {
           console.log(res)
         })
+      },
+      currentUser() {
+        console.log(BaaS.auth.currentUser())
+      },
+      requestPasswordReset() {
+        BaaS.auth.requestPasswordReset().then(res => {
+          console.log(res)
+        })
+      },
+      updatePassword() {
+        let user = BaaS.auth.currentUser()
+        user.updatePassword(_.pickBy(this.passwordChangeForm, v => !!v)).then(res => {
+          console.log(res)
+        })
+      },
+      updateUserinfo() {
+        let user = BaaS.auth.currentUser()
+        if (this.userInfoForm.username) {
+          user.updateUsername(this.userInfoForm.username).then(res => {
+            console.log(res)
+          })
+        } else if (this.userInfoForm.email) {
+          user.updateEmail(this.userInfoForm.email, true).then(res => {
+            console.log(res)
+          })
+        }
       }
     },
     mounted() {
-      BaaS.init('123')
+      window.BaaS.init('c2732ea16812760b8544')
     }
   })
 }
