aa89e3e6c88bcde8597245b5f152cea20f3682ac
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-03-28 16:51:07 +0800

    delete ticket-report-button; rename ticket-report-wrapper => ticket-report-wrapper

--- sdk-plugin/plugin/components/TicketReportButton/index.js
+++
@@ -1,9 +0,0 @@
-Component({
-  properties: {},
-  data: {},
-  methods: {
-    reportTicket: function (e) {
-      wx.BaaS.wxReportTicket(e.detail.formId)
-    },
-  },
-})
--- sdk-plugin/plugin/components/TicketReportWrapper/index.js
+++ sdk-plugin/plugin/components/TicketReportWrapper/index.js
@@ -1,5 +1,10 @@
 Component({
-  properties: {},
+  properties: {
+    disappearAfterClick: {
+      type: Boolean,
+      value: false,
+    }
+  },
   data: {
     tapped: false,
   },
