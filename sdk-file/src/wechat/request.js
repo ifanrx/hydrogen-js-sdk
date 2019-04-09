24f9b5aefeb83511107ce80a951e8b03389d7084
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-04-09 17:20:44 +0800

    lint

--- sdk-file/src/wechat/request.js
+++ sdk-file/src/wechat/request.js
@@ -37,7 +37,7 @@ const request = ({url, method = 'GET', data = {}, header = {}, dataType = 'json'
       header: headers,
       dataType: dataType,
       success: resolve,
-      fail: (res) => {
+      fail: () => {
         wxRequestFail(reject)
       }
     })
