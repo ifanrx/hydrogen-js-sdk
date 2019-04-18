62436069d66fcc3781b363c41fef947b657f5277
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-04-18 10:20:25 +0800

    修改插件中的测试应用

--- sdk-plugin/miniprogram/app.js
+++ sdk-plugin/miniprogram/app.js
@@ -4,7 +4,7 @@ App({
   onLaunch: function () {
     wx.BaaS = requirePlugin('sdkPlugin')
     wx.BaaS.wxExtend(wx.login, wx.getUserInfo, wx.requestPayment)
-    wx.BaaS.init('733b59d1b10ff4a37390')
+    wx.BaaS.init('733b59d1b10ff4a37390', {logLevel: 'debug', autoLogin: true})
 
     wx.BaaS.ErrorTracker.enable({usePlugins: true})
 
