724ecb14e6613260ab00343d37da63ff2b13759f
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-10-16 17:50:24 +0800

    modify package.json

--- test/web-dev-server/utils/common.js
+++ test/web-dev-server/utils/common.js
@@ -8,15 +8,15 @@
   }
 
   // 初始化
-  // window.BaaS.init('733b59d1b10ff4a37390', {
-  //   logLevel: 'debug',
-  // })
+  window.BaaS.init('a4d2d62965ddb57fa4d6', {
+    logLevel: 'debug',
+  })
 
-  // DEBUG config
-  BaaS._config.DEBUG = true;
-  BaaS.init('995140f59511a222c937', {logLevel: 'debug'});
-  BaaS._config.API_HOST = 'https://viac2-p.eng-vm.can.corp.ifanr.com';
-  BaaS._config.API_HOST_PATTERN = /^https:\/\/[\w-.]+\.ifanr\.com/;
+  // // DEBUG config
+  // BaaS._config.DEBUG = true;
+  // BaaS.init('995140f59511a222c937', {logLevel: 'debug'});
+  // BaaS._config.API_HOST = 'https://viac2-p.eng-vm.can.corp.ifanr.com';
+  // BaaS._config.API_HOST_PATTERN = /^https:\/\/[\w-.]+\.ifanr\.com/;
 
   // hook request
   var r = window.BaaS.request;
