3623486559d11cc9dea367beeb673c95dffde0c6
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-12-02 17:29:09 +0800

    增加注释说明

--- core/utils/withRetry.js
+++ core/utils/withRetry.js
@@ -10,6 +10,19 @@ const shouldRetry = (err, matchMessage) => {
   return err.message === matchMessage
 }
 
+/*
+ * 如果 fn 抛出错误，则继续重试调用。
+ * 重试次数不超过 maxCount - 1 次。
+ * 如果不指定 matchMessage，则抛出任何错误都重试。
+ * 如果指定了 matchMessage，则只重试特定的错误。
+ *
+ * @params {function} fn 需要重试的方法
+ * @params {object} options 其他选项
+ * @params {object} options.context 执行上下文
+ * @params {number} options.maxCount 最大执行次数
+ * @params {string|RegExp} options.matchMessage 错误信息匹配规则
+ * @return {*} 返回调用 fn 的返回值
+ */
 const withRetry = (fn, { context, maxCount = 10, matchMessage } = {}) => (...args) => {
   let remaindCount = maxCount
   let retry = () => {
