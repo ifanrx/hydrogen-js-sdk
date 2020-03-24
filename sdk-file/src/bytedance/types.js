23596a90eb5aa2bbb5d6e2710caded6f7e74452c
zhangzeshuan <zhangzeshuan@ifanr.com>
2020-03-24 17:42:57 +0800

    修改类型测试报错的问题

---
+++ sdk-file/src/bytedance/types.js
@@ -0,0 +1,8 @@
+/**
+ * 网络请求返回值
+ * @typedef {object.<T>} Response<T>
+ * @memberof BaaS
+ * @property {object} header - HTTP Response Header
+ * @property {T} data - 数据
+ * @property {number} statusCode - HTTP 状态码
+ */
