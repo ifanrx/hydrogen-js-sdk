8dac7607318dfd20748ba049a0a23296810ad55f
zhangzeshuan <zhangzeshuan@ifanr.com>
2020-04-22 18:09:57 +0800

    修改 webpack 配置

--- sdk-file/webpack/copyFilesForDev.default.js
+++ sdk-file/webpack/copyFilesForDev.default.js
@@ -6,4 +6,8 @@ module.exports = [
     from: isDEV ? 'sdk-web.dev.js' : `sdk-web.${pkg.version}.js`,
     to: '../../test/web-dev-server/sdk.dev.js',
   },
+  {  // 复制 wechat sdk 文件至测试目录
+    from: isDEV ? 'sdk-react-native.dev.js' : `sdk-react-native.${pkg.version}.js`,
+    to: path.resolve(process.env.HOME, './ifanr/hydrogen-js-sdk/react-native-minapp-sdk/sdk.js'),
+  },
 ]
