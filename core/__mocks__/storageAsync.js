a927b45c94bb4c9c2405128d1c4f3a16ae75f2b5
zhangzeshuan <zhangzeshuan@ifanr.com>
2020-05-13 17:49:49 +0800

    修改单元测试

--- core/__mocks__/storageAsync.js
+++ core/__mocks__/storageAsync.js
@@ -1,7 +1,28 @@
+const faker = require('faker')
+
+faker.seed(123)
+
+const cache = {}
+
 module.exports = {
   get: jest.fn().mockImplementation(key => {
-    if (key === 'uid') return Promise.resolve(10001)
-    if (key === 'auth_token') return Promise.resolve('abc')
-    if (key === 'session_expires_at') return Promise.resolve(Date.now() + 300000)
-  })
+    if (!cache[key]) {
+      switch(key) {
+      case 'uid':
+        cache[key] = faker.random.number()
+        break
+      case 'auth_token':
+        cache[key] = faker.random.uuid()
+        break
+      case 'session_expires_at':
+        cache[key] = Date.now() + faker.random.number() * 1000 // 默认生成一个未过期的时间
+        break
+      }
+    }
+    return Promise.resolve(cache[key])
+  }),
+  set: function(key, value) {
+    cache[key] = value
+    return Promise.resolve()
+  }
 }
---
+++ core/utils/__tests__/index.test.js
@@ -0,0 +1,96 @@
+const utils = require('../index')
+const BaaS = require('../../baas')
+
+jest.mock('../../baas')
+
+describe('utils', () => {
+  it('#log', () => {
+    expect(utils.log).toEqual(expect.any(Function))
+  })
+
+  it('#format', () => {
+    let url = '/hserve/v1/table/:tableID/'
+    let params = {
+      tableID: 10
+    }
+    expect(utils.format(url, params)).toBe('/hserve/v1/table/10/')
+
+    url = '/hserve/v1/table/:tableID/record/:recordID/'
+    params = {
+      tableID: 10,
+      recordID: 20
+    }
+    expect(utils.format(url, params)).toBe('/hserve/v1/table/10/record/20/')
+
+    params = {
+      tableID: 10,
+      recordID: 20,
+      ex: 30
+    }
+
+    expect(utils.format(url, params)).toBe('/hserve/v1/table/10/record/20/')
+  })
+
+  it('#parseRegExp', () => {
+    let regExpString = '^[a-zA-Z]+[0-9]*\\W?_$'
+    let result = utils.parseRegExp(new RegExp(regExpString, 'gi'))
+
+    expect(result).toEqual([regExpString, 'gi'])
+  })
+
+  it('replaceQueryParams', () => {
+    const requestParams = {
+      categoryID: 12,
+      otherData: 100
+    }
+    let result = utils.replaceQueryParams(requestParams)
+    expect(result).toEqual({'category_id': 12, otherData: 100})
+  })
+
+  it('compareVersion', () => {
+    let versions = [
+      ['v1.15.1', 'v1.15.2', -1],
+      ['1.15', 'v1.15.2', -1],
+      ['v1.15.1', '1.15.2', -1],
+      ['2.9.2', 'v1.16.1', 1],
+      ['1.16.1-a', 'v1.16.1-b', 0],
+      ['v1.15.1a', '1.15.1', 0],
+      ['v1.15', '1.15.0', 0],
+    ]
+
+    for (let i = 0; i < versions.length; i++) {
+      expect(utils.compareVersion(versions[i][0], versions[i][1])).toEqual(versions[i][2])
+    }
+  })
+
+  it('#doCreateRequestMethod()', () => {
+    BaaS._baasRequest = jest.fn().mockResolvedValue()
+    const METHOD_MAP_LIST = {
+      fakeFunction: {
+        url: '/hserve/v2.0/table/:tableID/record/?limit=:limit&offset=:offset&where=:where&enable_trigger=:enable_trigger',
+        method: 'PUT'
+      },
+    }
+    utils.doCreateRequestMethod(METHOD_MAP_LIST)
+    return BaaS.fakeFunction({
+      tableID: 12345,
+      where: JSON.stringify({'$and':[{'state':{'$contains':'一一'}}]}),
+      limit: 10,
+      offset: 5,
+      enable_trigger: 1,
+      data: {
+        a: 10,
+        b: 20,
+      }
+    }).then(() => {
+      expect(BaaS._baasRequest.mock.calls[0][0]).toEqual({
+        url: '/hserve/v2.0/table/12345/record/?limit=10&offset=5&where=%7B%22%24and%22%3A%5B%7B%22state%22%3A%7B%22%24contains%22%3A%22%E4%B8%80%E4%B8%80%22%7D%7D%5D%7D&enable_trigger=1',
+        method: 'PUT',
+        data: {
+          a: 10,
+          b: 20,
+        },
+      })
+    })
+  })
+})
