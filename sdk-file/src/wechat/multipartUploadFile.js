c19f99b1f0d0aceb23f5a82a5373c3ad5d1c4091
Jiajun Yan <yanjiajun@ifanr.com>
2023-11-02 18:36:51 +0800

    feat: 微信小程序 SDK 断点续传添加 onProgressUpdate 方法

--- sdk-file/src/wechat/multipartUploadFile.js
+++ sdk-file/src/wechat/multipartUploadFile.js
@@ -3,11 +3,11 @@ const HError = require('core-module/HError')
 const utils = require('core-module/utils')
 const storage = require('core-module/storage')
 const dayjs = require('dayjs')
-const {multipartUpload} = require('core-module/upload')
+const { multipartUpload } = require('core-module/upload')
 const SparkMD5 = require('spark-md5')
 
 const storageKey = constants.STORAGE_KEY.MULTIPART_UPLOAD
-const {getAuthorization, init, complete} = multipartUpload
+const { getAuthorization, init, complete } = multipartUpload
 
 const createFileChunks = async fileParams => {
   const chunkSize = 1 * 1024 * 1024 // 又拍云限制每次只能上传 1MB
@@ -26,7 +26,7 @@ const createFileChunks = async fileParams => {
       length,
     })
 
-    chunks.push({data: chunk.data, length})
+    chunks.push({ data: chunk.data, length })
     current = current + chunkSize
   }
 
@@ -63,7 +63,7 @@ const multipartStorage = {
  * @param {string} type 文件类型
  * @return {Promise<any>}
  */
-const multipartUploadFile = async (fileParams, metaData) => {
+const multipartUploadFile = (fileParams, metaData) => {
   if (
     !fileParams ||
     typeof fileParams !== 'object' ||
@@ -86,11 +86,7 @@ const multipartUploadFile = async (fileParams, metaData) => {
     throw new HError(605)
   }
 
-  const wxRequest = utils.promisify(wx.request)
-  const chunks = await createFileChunks(fileParams)
-  const md5 = SparkMD5.ArrayBuffer.hash(chunks)
-
-  const getUploadRecord = () => {
+  const getUploadRecord = md5 => {
     const now = dayjs()
     const uploadRecord = multipartStorage.get(md5) || {}
 
@@ -103,130 +99,186 @@ const multipartUploadFile = async (fileParams, metaData) => {
     return null
   }
 
-  const initMultipartUpload = async () => {
-    const uploadRecord = getUploadRecord()
+  let rs, rj, uploadCallback = null
+  // isAborted
 
-    // 有上传记录，则续传
-    if (uploadRecord) {
-      const res = await getAuthorization(uploadRecord.id)
-      const initConfig = {...res.data, ...uploadRecord}
+  let p = new Promise((resolve, reject) => {
+    rs = resolve
+    rj = reject
+  })
+
+  function onProgressUpdate(cb) {
+    uploadCallback = cb
+  }
+
+  function mix(obj) {
+    return Object.assign(obj, {
+      catch(...args) {
+        let newPromise = Promise.prototype.catch.call(this, ...args)
+        mix(newPromise)
+        return newPromise
+      },
+      then(...args) {
+        let newPromise = Promise.prototype.then.call(this, ...args)
+        mix(newPromise)
+        return newPromise
+      },
+      onProgressUpdate,
+    })
+  }
+
+  mix(p)
+
+  const wxRequest = utils.promisify(wx.request)
+
+  createFileChunks(fileParams).then(async chunks => {
+    const md5 = SparkMD5.ArrayBuffer.hash(chunks)
+
+    const initMultipartUpload = async () => {
+      const uploadRecord = getUploadRecord(md5)
+
+      // 有上传记录，则续传
+      if (uploadRecord) {
+        const res = await getAuthorization(uploadRecord.id)
+        const initConfig = { ...res.data, ...uploadRecord }
+        return initConfig
+      }
+
+      const filename =
+        fileParams.fileName || utils.getFileNameFromPath(fileParams.filePath)
+      const data = { file_size: fileParams.fileSize, filename }
+      const res = await init(data, utils.replaceQueryParams(metaData))
+      const initConfig = res.data
+
+      // 超时或者初始上传，都重新设置新值
+      multipartStorage.set(md5, {
+        init_time: new Date().getTime(),
+        multi_part_id: +initConfig.multi_part_id,
+        multi_uuid: initConfig.multi_uuid,
+        upload_url: initConfig.upload_url,
+        id: initConfig.id,
+      })
+
       return initConfig
     }
 
-    const filename =
-      fileParams.fileName || utils.getFileNameFromPath(fileParams.filePath)
-    const data = {file_size: fileParams.fileSize, filename}
-    const res = await init(data, utils.replaceQueryParams(metaData))
-    const initConfig = res.data
+    const multipartUpload = async data => {
+      const _chunks = chunks.slice(data.multi_part_id)
+      let uuid = data.multi_uuid
+      let nextPartId = data.multi_part_id
 
-    // 超时或者初始上传，都重新设置新值
-    multipartStorage.set(md5, {
-      init_time: new Date().getTime(),
-      multi_part_id: +initConfig.multi_part_id,
-      multi_uuid: initConfig.multi_uuid,
-      upload_url: initConfig.upload_url,
-      id: initConfig.id,
-    })
-
-    return initConfig
-  }
-
-  const multipartUpload = async data => {
-    const _chunks = chunks.slice(data.multi_part_id)
-    let uuid = data.multi_uuid
-    let nextPartId = data.multi_part_id
-
-    const uploadChunk = async chunk => {
-      const res = await wxRequest({
-        url: data.upload_url,
-        method: 'PUT',
-        data: chunk.data,
-        header: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': chunk.length,
-          Authorization: data.authorization,
-          'x-date': data.date,
-          'x-upyun-multi-stage': 'upload',
-          'x-upyun-multi-uuid': uuid,
-          'x-upyun-part-id': nextPartId,
-        },
-      })
-
-      const stringifiedStatusCode = res.statusCode + ''
-
-      if (!stringifiedStatusCode.startsWith('2')) {
-        // 如果是 authorization 超时 30 分钟，则需重新获取
-        if (res && res.data && res.data.code === 40100002) {
-          const uploadRecord = getUploadRecord()
-          const res = await getAuthorization(uploadRecord.id)
-          data.authorization = res.data.authorization
-          data.date = res.data.date
-          return await uploadChunk(chunk)
-        }
-
-        throw res
+      if (uploadCallback) {
+        uploadCallback({
+          progress: Math.floor((nextPartId / chunks.length) * 100),
+        })
       }
 
-      return res
+      const uploadChunk = async chunk => {
+        const res = await wxRequest({
+          url: data.upload_url,
+          method: 'PUT',
+          data: chunk.data,
+          header: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': chunk.length,
+            Authorization: data.authorization,
+            'x-date': data.date,
+            'x-upyun-multi-stage': 'upload',
+            'x-upyun-multi-uuid': uuid,
+            'x-upyun-part-id': nextPartId,
+          },
+        })
+
+        const stringifiedStatusCode = res.statusCode + ''
+
+        if (!stringifiedStatusCode.startsWith('2')) {
+          // 如果是 authorization 超时 30 分钟，则需重新获取
+          if (res && res.data && res.data.code === 40100002) {
+            const uploadRecord = getUploadRecord(md5)
+            const res = await getAuthorization(uploadRecord.id)
+            data.authorization = res.data.authorization
+            data.date = res.data.date
+            return await uploadChunk(chunk)
+          }
+
+          throw res
+        }
+
+        return res
+      }
+
+      for (let chunk of _chunks) {
+        const res = await uploadChunk(chunk)
+
+        uuid = res.header['x-upyun-multi-uuid']
+        nextPartId = res.header['x-upyun-next-part-id']
+
+        multipartStorage.set(md5, {
+          multi_part_id: +nextPartId,
+          multi_uuid: uuid,
+        }) // 保存当前上传记录
+
+        if (nextPartId == -1) break
+
+        if (uploadCallback) {
+          uploadCallback({
+            progress: Math.floor((nextPartId / chunks.length) * 100),
+          })
+        }
+      }
+
+      return { file: data, multi_uuid: uuid }
     }
 
-    for (let chunk of _chunks) {
-      const res = await uploadChunk(chunk)
+    const completeMultipartUpload = async data => {
+      const uploadRecord = getUploadRecord(md5)
 
-      uuid = res.header['x-upyun-multi-uuid']
-      nextPartId = res.header['x-upyun-next-part-id']
+      return complete(uploadRecord.id, data.multi_uuid).then(res => {
+        if (res.data.upload_status !== 'success') {
+          throw new HError(617)
+        }
 
-      multipartStorage.set(md5, {
-        multi_part_id: +nextPartId,
-        multi_uuid: uuid,
-      }) // 保存当前上传记录
+        multipartStorage.delete(md5) // 上传成功，删除上传记录
 
-      if (nextPartId === -1) break
+        if (uploadCallback) {
+          uploadCallback({ progress: 100 })
+        }
+
+        return {
+          data: {
+            status: 'ok',
+            path: data.file.path,
+            file: {
+              id: data.file.id,
+              path: data.file.path,
+              name: data.file.name,
+              created_at: data.file.created_at,
+              // mime_type: fileObj.type, // 又拍云没有提供
+              cdn_path: data.file.cdn_path,
+              size: fileParams.fileSize,
+            },
+          },
+        }
+      })
     }
 
-    return {file: data, multi_uuid: uuid}
-  }
-
-  const completeMultipartUpload = async data => {
-    const uploadRecord = getUploadRecord()
-
-    return complete(uploadRecord.id, data.multi_uuid).then(res => {
-      if (res.data.upload_status !== 'success') {
-        throw new HError(617)
+    try {
+      const initConfig = await initMultipartUpload()
+      const data = await multipartUpload(initConfig)
+      const result = await completeMultipartUpload(data)
+      return rs(result)
+    } catch (error) {
+      // 没有 statusCode 返回，一般是网络问题，不做删除处理
+      if (!error.statusCode) {
+        rj(new HError(600))
       }
 
       multipartStorage.delete(md5) // 上传成功，删除上传记录
-      return {
-        data: {
-          status: 'ok',
-          path: data.file.path,
-          file: {
-            id: data.file.id,
-            path: data.file.path,
-            name: data.file.name,
-            created_at: data.file.created_at,
-            // mime_type: fileObj.type, // 又拍云没有提供
-            cdn_path: data.file.cdn_path,
-            size: fileParams.fileSize,
-          },
-        },
-      }
-    })
-  }
-
-  try {
-    const initConfig = await initMultipartUpload()
-    const data = await multipartUpload(initConfig)
-    return await completeMultipartUpload(data)
-  } catch (error) {
-    // 没有 statusCode 返回，一般是网络问题，不做删除处理
-    if (!error.statusCode) {
-      throw new HError(600)
+      rj(error)
     }
+  }, rj)
 
-    multipartStorage.delete(md5) // 上传成功，删除上传记录
-    throw error
-  }
+  return p
 }
 
 module.exports = multipartUploadFile
